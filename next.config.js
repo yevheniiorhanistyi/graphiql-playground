0efeeebabdd0cef8d5237ae84a6ed32a0064e660
Vladimir <v.milashevsky@gmail.com>
2023-12-07 23:42:22 +0300

    feat: add breakpoints variables

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+import path from 'path';
+
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join('./src/', 'styles')],
+    prependData: `@import "breakpoints.scss";`,
+  },
 };
 
 export default nextConfig;
