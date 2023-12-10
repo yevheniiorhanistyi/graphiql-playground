cceac61702eb91f98246f208cb56d358f979cae7
Vladimir <v.milashevsky@gmail.com>
2023-12-10 14:23:25 +0300

    fix: fix form schema validation

--- src/utils/FormsSchema.ts
+++ src/utils/FormsSchema.ts
@@ -12,16 +12,16 @@ export const signUpFormSchema = signInFormSchema.shape({
   password: yup
     .string()
     .required('Please enter password')
+    .matches(
+      /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9])(?=.*[?^\x00-\x7F]).{8,32}$/,
+      'Password is not strong enough. Please use a stronger one'
+    )
     .min(8, 'Password should be more than 8 characters')
     .max(32, 'Password should be less than 32 characters')
     .matches(/[A-Z]/, 'Password should contain at least 1 uppercase letter')
     .matches(/[a-z]/, 'Password should contain at least 1 lowercase letter')
     .matches(/[0-9]/, 'Password should contain at least 1 number')
-    .matches(/[@$!%*?&]/, 'Password should contain at least 1 special character')
-    .matches(
-      /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9])(?=.*[?^\x00-\x7F]).{8,}$/,
-      'Password is not strong enough. Please use a stronger one'
-    ),
+    .matches(/[@$!%*?&]/, 'Password should contain at least 1 special character'),
   passwordConfirmation: yup
     .string()
     .required('Please repeat the password')
