f1da88068804149d98f2d4f65ebf900071b5f066
Yevhenii Orhanistyi <organistiy@gmail.com>
2023-12-12 17:01:32 +0100

    feat: add customStyles prop

--- src/components/common/BasicButton/BasicButton.tsx
+++ src/components/common/BasicButton/BasicButton.tsx
@@ -1,15 +1,17 @@
-import { FC, ButtonHTMLAttributes, ReactNode } from 'react';
+import { FC, ButtonHTMLAttributes, ReactNode, CSSProperties } from 'react';
 import styles from './BasicButton.module.scss';
 
 interface BasicButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   active?: boolean;
+  customStyles?: CSSProperties;
 }
 
-const BasicButton: FC<BasicButtonProps> = ({ children, active, ...props }) => (
+const BasicButton: FC<BasicButtonProps> = ({ children, active, customStyles, ...props }) => (
   <button
     {...props}
     className={`${styles.button} ${active ? styles.active : ''}`}
+    style={customStyles}
     type={props.type}
   >
     <span className={styles.button_label}>{children}</span>
