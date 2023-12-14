1134d76e5214779e899f27a093d16d56d24129a8
Vladimir <v.milashevsky@gmail.com>
2023-12-15 02:38:22 +0300

    fix: fix show query, mutaion, subscription types

--- src/utils/graphQL_API/initialGraphQLRequest.tsx
+++ src/utils/graphQL_API/initialGraphQLRequest.tsx
@@ -14,5 +14,31 @@ export const initialGraphQLRequest = async (endpoint: string) => {
 
   const data: dataInterface = await response.json();
 
-  console.log(data.data.__schema.types.filter((type) => !type.name?.includes('__')));
+  console.log('Full schema: ', data.data.__schema);
+
+  const queryName = data.data.__schema.queryType.name;
+
+  const queries = data.data.__schema.types.filter((type) => type.name === queryName);
+
+  console.log('Queries: ', queries[0].fields);
+
+  if (data.data.__schema.mutationType) {
+    const mutationName = data.data.__schema.mutationType?.name;
+
+    const mutations = data.data.__schema.types.filter((type) => type.name === mutationName);
+
+    console.log('Mutations: ', mutations[0].fields);
+  } else {
+    console.log('No mutations for this API');
+  }
+
+  if (data.data.__schema.subscriptionType) {
+    const subscriptionName = data.data.__schema.subscriptionType?.name;
+
+    const subscriptions = data.data.__schema.types.filter((type) => type.name === subscriptionName);
+
+    console.log('Subscriptions: ', subscriptions[0].fields);
+  } else {
+    console.log('No subscriptions for this API');
+  }
 };
