f50a525bd43ec3909e07a26103ed60bc1d61bdfe
Yevhenii Orhanistyi <organistiy@gmail.com>
2023-12-15 09:19:27 +0100

    feat: add About component

--- src/pages/welcome/index.tsx
+++ src/pages/welcome/index.tsx
@@ -1,12 +1,14 @@
 import { FC } from 'react';
 import Home from '@/components/Home/Home';
 import Features from '@/components/Features/Features';
+import About from '@/components/About/About';
 
 const Welcome: FC = () => {
   return (
     <>
       <Home />
       <Features />
+      <About />
     </>
   );
 };
