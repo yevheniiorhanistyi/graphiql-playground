eeb5627acde2199e1598197c70de574eea5c0917
Yevhenii Orhanistyi <organistiy@gmail.com>
2023-12-27 11:33:18 +0100

    feat: add tests for findGraphQLType function

---
+++ tests/utils/graphQL_API/findGraphQLType.test.ts
@@ -0,0 +1,72 @@
+import { describe, test, expect } from 'vitest';
+import { __TypeKind } from '@/interfaces/schemaInterface';
+import findGraphQLType from '@/utils/graphQL_API/findGraphQLType';
+import { createMockSchemaObject, createMockSchemaString } from './mockSchema/mockSchema';
+
+describe('findGraphQLType function', () => {
+  test('returns null for non-existing type', () => {
+    const schema = createMockSchemaString();
+    const result = findGraphQLType(schema, 'NonExistingType');
+    expect(result).toBeNull();
+  });
+
+  test('returns type for existing scalar type', () => {
+    const schema = createMockSchemaString();
+    const result = findGraphQLType(schema, 'String');
+    expect(result).toEqual({
+      kind: __TypeKind.SCALAR,
+      name: 'String',
+      description: 'Scalar type representing a string',
+      fields: null,
+      interfaces: null,
+      possibleTypes: null,
+      enumValues: null,
+      inputFields: null,
+      ofType: null,
+      specifiedByURL: null,
+    });
+  });
+
+  test('return null if typeName is null', () => {
+    const schema = createMockSchemaString();
+    const result = findGraphQLType(schema, null);
+    expect(result).toBe(null);
+  });
+
+  test('returns type for existing object type', () => {
+    const schema = createMockSchemaObject();
+    const result = findGraphQLType(schema, 'User');
+    expect(result).toEqual({
+      kind: __TypeKind.OBJECT,
+      name: 'User',
+      description: 'User type',
+      fields: [
+        {
+          name: 'id',
+          description: 'User ID',
+          args: [],
+          type: {
+            kind: __TypeKind.SCALAR,
+            name: 'ID',
+            description: 'Scalar type representing an ID',
+            fields: null,
+            interfaces: null,
+            possibleTypes: null,
+            enumValues: null,
+            inputFields: null,
+            ofType: null,
+            specifiedByURL: null,
+          },
+          isDeprecated: false,
+          deprecationReason: null,
+        },
+      ],
+      interfaces: null,
+      possibleTypes: null,
+      enumValues: null,
+      inputFields: null,
+      ofType: null,
+      specifiedByURL: null,
+    });
+  });
+});
