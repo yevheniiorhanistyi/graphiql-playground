6a0f3e9d61c3022a39b0b9c226b86a74ea3f7ee5
Yevhenii Orhanistyi <organistiy@gmail.com>
2023-12-27 12:39:01 +0100

    feat: add tests for DocsInputValue component

---
+++ tests/components/Docs/DocsInputValue/DocsInputValue.test.tsx
@@ -0,0 +1,40 @@
+import { vi, describe, test, expect } from 'vitest';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { __InputValue } from '@/interfaces/schemaInterface';
+import LocaleProvider from '@/localization/LocaleProvider';
+import DocsInputValue from '@/components/Docs/DocsInputValue/DocsInputValue';
+
+const mockInputValue = {
+  name: 'name',
+  description: '',
+  type: {
+    kind: 'SCALAR',
+    name: 'String',
+    ofType: null,
+  },
+  defaultValue: null,
+} as __InputValue;
+
+const handleClickValueMock = vi.fn();
+
+describe('DocsInputValue component', () => {
+  test('renders DocsInputValue component with mock input value', () => {
+    render(
+      <LocaleProvider>
+        <DocsInputValue inputValue={mockInputValue} handleClickValue={handleClickValueMock} />
+      </LocaleProvider>
+    );
+
+    expect(screen.getByText('name')).toBeInTheDocument();
+    expect(screen.getByText('Type')).toBeInTheDocument();
+    expect(screen.getByText('String')).toBeInTheDocument();
+  });
+
+  test('calls handleClickValue when type is clicked', () => {
+    render(<DocsInputValue inputValue={mockInputValue} handleClickValue={handleClickValueMock} />);
+
+    fireEvent.click(screen.getByText('String'));
+
+    expect(handleClickValueMock).toHaveBeenCalledWith(mockInputValue.type);
+  });
+});
