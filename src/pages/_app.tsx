9ccf0c576471e242598c904e9c46fdfdd1531d55
Vladimir <v.milashevsky@gmail.com>
2024-01-04 02:50:40 +0300

    refactor: delete layout when 404 is shown

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,17 +5,37 @@ import '@/styles/globals.scss';
 import '@/styles/variables.scss';
 import ThemeProvider from '@/theme/ThemeProvider';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { Routes } from '@/constants/enums';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider>
-      <LocaleProvider>
-        <AuthContextProvider>
-          <Layout>
+  const router = useRouter();
+
+  if (
+    router.pathname === Routes.WELCOME_PAGE ||
+    router.pathname === `/${Routes.PLAYGROUND_PAGE}` ||
+    router.pathname === `/${Routes.AUTH_PAGE}`
+  ) {
+    return (
+      <ThemeProvider>
+        <LocaleProvider>
+          <AuthContextProvider>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </AuthContextProvider>
+        </LocaleProvider>
+      </ThemeProvider>
+    );
+  } else {
+    return (
+      <ThemeProvider>
+        <LocaleProvider>
+          <AuthContextProvider>
             <Component {...pageProps} />
-          </Layout>
-        </AuthContextProvider>
-      </LocaleProvider>
-    </ThemeProvider>
-  );
+          </AuthContextProvider>
+        </LocaleProvider>
+      </ThemeProvider>
+    );
+  }
 }
