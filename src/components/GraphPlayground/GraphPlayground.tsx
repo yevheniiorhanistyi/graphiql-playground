0c0f8aa52cafb0b79e119f687de06d406829af51
Stanislav <stanislavstranger@gmail.com>
2024-01-04 17:50:24 +0300

    fix: fix scss in GraphPlayground

--- src/components/GraphPlayground/GraphPlayground.tsx
+++ src/components/GraphPlayground/GraphPlayground.tsx
@@ -140,16 +140,28 @@ const GraphiQLPage = () => {
       )}
 
       <div className={styles.playgroynd_container}>
-        <div className={styles.left_part}>
-          <InputEndpoint getEndpoint={setEndpoint} error={isEndpointCorrect} />
-
+        <div className={styles.input_part}>
+          <InputEndpoint getEndpoint={setEndpoint} error={isEndpointCorrect} />{' '}
+          <button className={styles.send_button} onClick={handleExecuteQuery}>
+            <svg width="24" height="24" viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg">
+              <path
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M5.82 11L8 13.17a1.1 1.1 0 0 0 1.05.3a1.12 1.12 0 0 0 .81-.74L13.44 2A1.12 1.12 0 0 0 12 .56L1.27 4.14A1.12 1.12 0 0 0 .53 5a1.1 1.1 0 0 0 .3 1l2.74 2.74l-.09 3.47ZM13.12.78L3.57 8.74"
+              />
+            </svg>
+          </button>
+        </div>
+        <div className={styles.textEditor_part}>
           <TextArea
             value={query}
             onChange={handleQueryChange}
             schema={schema}
             placeholder={t['Enter code here...']}
           />
-
+        </div>
+        <div className={styles.variables_part}>
           <div>
             <BasicButton onClick={() => setSelectedTab('variables')}>Variables</BasicButton>
             <BasicButton onClick={() => setSelectedTab('headers')}>Headers</BasicButton>
@@ -171,22 +183,14 @@ const GraphiQLPage = () => {
           </div>
         </div>
 
-        <button className={styles.send_button} onClick={handleExecuteQuery}>
-          <svg width="24" height="24" viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg">
-            <path
-              fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M5.82 11L8 13.17a1.1 1.1 0 0 0 1.05.3a1.12 1.12 0 0 0 .81-.74L13.44 2A1.12 1.12 0 0 0 12 .56L1.27 4.14A1.12 1.12 0 0 0 .53 5a1.1 1.1 0 0 0 .3 1l2.74 2.74l-.09 3.47ZM13.12.78L3.57 8.74"
-            />
-          </svg>
-        </button>
-
-        <TextArea
-          value={response}
-          readOnly={true}
-          placeholder={t['Submit a request to get the result in this field...']}
-        />
+        <div className={styles.result_part}>
+          <TextArea
+            value={response}
+            readOnly={true}
+            placeholder={t['Submit a request to get the result in this field...']}
+          />
+        </div>
+        <div className={styles.docs_part}> </div>
       </div>
 
       <BasicButton disabled={disableDocsBtn} onClick={toggleDocsDisplayed}>
