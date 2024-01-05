b19a97ad2d0d088e19be9af944e330afd8636244
Stanislav <stanislavstranger@gmail.com>
2024-01-06 01:59:14 +0300

    fix: fix handleCursorPosition

--- src/components/GraphPlayground/GraphPlayground.tsx
+++ src/components/GraphPlayground/GraphPlayground.tsx
@@ -142,21 +142,8 @@ const GraphiQLPage = () => {
             error={isEndpointCorrect}
             setIsEndpointCorrect={setIsEndpointCorrect}
           />
-          <button
-            className={styles.send_button}
-            onClick={handleExecuteQuery}
-            disabled={!isEndpointCorrect}
-          >
-            <svg width="24" height="24" viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg">
-              <path
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M5.82 11L8 13.17a1.1 1.1 0 0 0 1.05.3a1.12 1.12 0 0 0 .81-.74L13.44 2A1.12 1.12 0 0 0 12 .56L1.27 4.14A1.12 1.12 0 0 0 .53 5a1.1 1.1 0 0 0 .3 1l2.74 2.74l-.09 3.47ZM13.12.78L3.57 8.74"
-              />
-            </svg>
-          </button>
         </div>
+
         <div className={styles.row}>
           <div className={styles.column}>
             <div className={styles.textEditor_part}>
@@ -166,6 +153,20 @@ const GraphiQLPage = () => {
                 schema={schema}
                 placeholder={t['Enter code here...']}
               />
+              <button
+                className={styles.send_button}
+                onClick={handleExecuteQuery}
+                disabled={!isEndpointCorrect}
+              >
+                <svg width="22" height="22" viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    fill="none"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M5.82 11L8 13.17a1.1 1.1 0 0 0 1.05.3a1.12 1.12 0 0 0 .81-.74L13.44 2A1.12 1.12 0 0 0 12 .56L1.27 4.14A1.12 1.12 0 0 0 .53 5a1.1 1.1 0 0 0 .3 1l2.74 2.74l-.09 3.47ZM13.12.78L3.57 8.74"
+                  />
+                </svg>
+              </button>
             </div>
 
             <div className={styles.variables_part}>
