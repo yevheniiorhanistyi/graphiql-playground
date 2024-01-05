66803badbfd42df2814954c0f65f4d5b01a26b2c
Stanislav <stanislavstranger@gmail.com>
2024-01-05 19:56:46 +0300

    feat: add prepareQuery for TextArea in Tabs

--- src/components/GraphPlayground/GraphPlayground.tsx
+++ src/components/GraphPlayground/GraphPlayground.tsx
@@ -1,4 +1,3 @@
-import BasicButton from '@/components/common/BasicButton/BasicButton';
 import styles from './GraphPlayground.module.scss';
 import InputEndpoint from '../InputEndpoint/InputEndpoint';
 import ErrorToast from '@/components/ErrorToast/ErrorToast';
@@ -87,7 +86,8 @@ const GraphiQLPage = () => {
 
       if (variables) {
         try {
-          const variablesObj = JSON.parse(variables);
+          const variablesWithDoubleQuotes = prepareQuery(variables);
+          const variablesObj = JSON.parse(variablesWithDoubleQuotes);
           requestBody.variables = variablesObj;
         } catch (error) {
           console.error('Error parsing variables JSON:', error);
@@ -97,7 +97,8 @@ const GraphiQLPage = () => {
       let headersObj = {};
       if (headers) {
         try {
-          headersObj = JSON.parse(headers);
+          const headersWithDoubleQuotes = prepareQuery(headers);
+          headersObj = JSON.parse(headersWithDoubleQuotes);
         } catch (error) {
           console.error('Error parsing headers JSON:', error);
         }
@@ -139,7 +140,7 @@ const GraphiQLPage = () => {
         </Suspense>
       )}
 
-      <div className={styles.playgroynd_container}>
+      <div className={styles.playground_container}>
         <div className={styles.input_part}>
           <InputEndpoint getEndpoint={setEndpoint} error={isEndpointCorrect} />{' '}
           <button className={styles.send_button} onClick={handleExecuteQuery}>
@@ -153,49 +154,79 @@ const GraphiQLPage = () => {
             </svg>
           </button>
         </div>
-        <div className={styles.textEditor_part}>
-          <TextArea
-            value={query}
-            onChange={handleQueryChange}
-            schema={schema}
-            placeholder={t['Enter code here...']}
-          />
-        </div>
-        <div className={styles.variables_part}>
-          <div>
-            <BasicButton onClick={() => setSelectedTab('variables')}>Variables</BasicButton>
-            <BasicButton onClick={() => setSelectedTab('headers')}>Headers</BasicButton>
-            {selectedTab === 'variables' && (
+        <div className={styles.row}>
+          <div className={styles.column}>
+            <div className={styles.textEditor_part}>
               <TextArea
-                value={variables}
-                onChange={handleVariablesChange}
-                placeholder={t['Enter variables here...']}
+                value={query}
+                onChange={handleQueryChange}
+                schema={schema}
+                placeholder={t['Enter code here...']}
               />
-            )}
+            </div>
 
-            {selectedTab === 'headers' && (
-              <TextArea
-                value={headers}
-                onChange={handleHeadersChange}
-                placeholder={t['Enter headers here...']}
-              />
-            )}
+            <div className={styles.variables_part}>
+              <div>
+                <button
+                  className={`${styles.tab_button} ${
+                    selectedTab === 'variables' ? styles.active : ''
+                  }`}
+                  onClick={() => setSelectedTab('variables')}
+                >
+                  {t['Variables']}
+                </button>
+                <button
+                  className={`${styles.tab_button} ${
+                    selectedTab === 'headers' ? styles.active : ''
+                  }`}
+                  onClick={() => setSelectedTab('headers')}
+                >
+                  {t['Headers']}
+                </button>
+                {selectedTab === 'variables' && (
+                  <TextArea
+                    value={variables}
+                    onChange={handleVariablesChange}
+                    placeholder={t['Enter variables here...']}
+                  />
+                )}
+
+                {selectedTab === 'headers' && (
+                  <TextArea
+                    value={headers}
+                    onChange={handleHeadersChange}
+                    placeholder={t['Enter headers here...']}
+                  />
+                )}
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.result_part}>
+            <TextArea
+              value={response}
+              readOnly={true}
+              placeholder={t['Submit a request to get the result in this field...']}
+            />
           </div>
         </div>
-
-        <div className={styles.result_part}>
-          <TextArea
-            value={response}
-            readOnly={true}
-            placeholder={t['Submit a request to get the result in this field...']}
-          />
-        </div>
-        <div className={styles.docs_part}> </div>
       </div>
 
-      <BasicButton disabled={disableDocsBtn} onClick={toggleDocsDisplayed}>
-        {t['Show Docs']}
-      </BasicButton>
+      <button
+        className={styles.docs_button}
+        disabled={disableDocsBtn}
+        onClick={toggleDocsDisplayed}
+      >
+        <svg height="24px" viewBox="0 0 20 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <title>docs icon</title>
+          <path
+            d="M0.75 3C0.75 4.24264 1.75736 5.25 3 5.25H17.25M0.75 3C0.75 1.75736 1.75736 0.75 3 0.75H16.25C16.8023 0.75 17.25 1.19772 17.25 1.75V5.25M0.75 3V21C0.75 22.2426 1.75736 23.25 3 23.25H18.25C18.8023 23.25 19.25 22.8023 19.25 22.25V6.25C19.25 5.69771 18.8023 5.25 18.25 5.25H17.25"
+            stroke="currentColor"
+            strokeWidth="1.5"
+          ></path>
+          <line x1="13" y1="11.75" x2="6" y2="11.75" stroke="currentColor" strokeWidth="1.5"></line>
+        </svg>
+      </button>
     </>
   );
 };
--- src/components/UI/TextArea/TextArea.tsx
+++ src/components/UI/TextArea/TextArea.tsx
@@ -121,86 +121,84 @@ const TextArea: FC<TextAreaProps> = ({
   };
 
   return (
-    <div>
-      <div>
-        <div className={styles.text_editor}>
-          <textarea
-            id="lineCounter"
-            ref={lineCounterRef}
-            wrap="off"
-            readOnly
-            onClick={handleLineCounterClick}
-            className={styles.line_counter}
-          />
-          <textarea
-            id="codeEditor"
-            ref={codeEditorRef}
-            wrap="off"
-            value={code}
-            onChange={handleOnChange}
-            onBlur={handleOnChange}
-            onScroll={handleScroll}
-            onKeyDown={handleKeyDownEvent}
-            onClick={handleCursorPositionEvent}
-            className={styles.code_editor}
-            placeholder={placeholder}
-            readOnly={readOnly}
-          />
-          <div className={styles.tools}>
-            {!readOnly && (
-              <button onClick={handleClear}>
-                <svg width="24" height="24" viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    fill="none"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M1 3.5h12m-10.5 0h9v9a1 1 0 0 1-1 1h-7a1 1 0 0 1-1-1v-9h0Zm2 0V3a2.5 2.5 0 0 1 5 0v.5m-4 2V11m3-5.5V11"
-                  />
-                </svg>
-              </button>
-            )}
-            {!readOnly && (
-              <button onClick={() => handleCopy(code)}>
-                <svg width="24" height="24" viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg">
-                  <g fill="none" strokeLinecap="round" strokeLinejoin="round">
-                    <path d="M12.5 10a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V1.5a1 1 0 0 1 1-1h4.5l3 3Z" />
-                    <path d="M9.5 13.5h-7a1 1 0 0 1-1-1v-9" />
-                  </g>
-                </svg>
-              </button>
-            )}
-            {!readOnly && (
-              <button onClick={() => handlePaste(code, setCode, codeEditorRef)}>
-                <svg width="24" height="24" viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg">
-                  <g fill="none" strokeLinecap="round" strokeLinejoin="round">
-                    <path d="M8.5 3.5v-1a1 1 0 0 0-1-1h-1m-2.5 9H1.5a1 1 0 0 1-1-1v-7a1 1 0 0 1 1-1h1" />
-                    <rect width="7" height="8" x="6.5" y="5.5" rx="1" />
-                    <path d="M6.75.5h-4.5l.41 1.62a.49.49 0 0 0 .48.38h2.72a.49.49 0 0 0 .48-.38Zm1.75 8h3m-3 2h3" />
-                  </g>
-                </svg>
-              </button>
-            )}
-          </div>
-          {matches.length !== 0 && matches[0] !== '' && (
-            <SelectSnippet
-              matches={matches}
-              setMatches={setMatches}
-              cursorPosition={cursorPosition}
-              code={code}
-              setCode={setCode}
-              cursorCount={cursorCount}
-              inputValueRef={inputValueRef}
-              codeEditorRef={codeEditorRef}
-            />
+    <>
+      <div className={styles.text_editor}>
+        <textarea
+          id="lineCounter"
+          ref={lineCounterRef}
+          wrap="off"
+          readOnly
+          onClick={handleLineCounterClick}
+          className={styles.line_counter}
+        />
+        <textarea
+          id="codeEditor"
+          ref={codeEditorRef}
+          wrap="off"
+          value={code}
+          onChange={handleOnChange}
+          onBlur={handleOnChange}
+          onScroll={handleScroll}
+          onKeyDown={handleKeyDownEvent}
+          onClick={handleCursorPositionEvent}
+          className={styles.code_editor}
+          placeholder={placeholder}
+          readOnly={readOnly}
+        />
+        <div className={styles.tools}>
+          {!readOnly && (
+            <button onClick={handleClear}>
+              <svg width="24" height="24" viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  fill="none"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M1 3.5h12m-10.5 0h9v9a1 1 0 0 1-1 1h-7a1 1 0 0 1-1-1v-9h0Zm2 0V3a2.5 2.5 0 0 1 5 0v.5m-4 2V11m3-5.5V11"
+                />
+              </svg>
+            </button>
+          )}
+          {!readOnly && (
+            <button onClick={() => handleCopy(code)}>
+              <svg width="24" height="24" viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg">
+                <g fill="none" strokeLinecap="round" strokeLinejoin="round">
+                  <path d="M12.5 10a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V1.5a1 1 0 0 1 1-1h4.5l3 3Z" />
+                  <path d="M9.5 13.5h-7a1 1 0 0 1-1-1v-9" />
+                </g>
+              </svg>
+            </button>
+          )}
+          {!readOnly && (
+            <button onClick={() => handlePaste(code, setCode, codeEditorRef)}>
+              <svg width="24" height="24" viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg">
+                <g fill="none" strokeLinecap="round" strokeLinejoin="round">
+                  <path d="M8.5 3.5v-1a1 1 0 0 0-1-1h-1m-2.5 9H1.5a1 1 0 0 1-1-1v-7a1 1 0 0 1 1-1h1" />
+                  <rect width="7" height="8" x="6.5" y="5.5" rx="1" />
+                  <path d="M6.75.5h-4.5l.41 1.62a.49.49 0 0 0 .48.38h2.72a.49.49 0 0 0 .48-.38Zm1.75 8h3m-3 2h3" />
+                </g>
+              </svg>
+            </button>
           )}
         </div>
-        {!readOnly && (
-          <p
-            className={styles.info_line}
-          >{`Space: ${TAB_SPACES}, Ln ${cursorCount.row}, Col ${cursorCount.col}, Ch ${code.length}`}</p>
+        {matches.length !== 0 && matches[0] !== '' && (
+          <SelectSnippet
+            matches={matches}
+            setMatches={setMatches}
+            cursorPosition={cursorPosition}
+            code={code}
+            setCode={setCode}
+            cursorCount={cursorCount}
+            inputValueRef={inputValueRef}
+            codeEditorRef={codeEditorRef}
+          />
         )}
       </div>
-    </div>
+      {!readOnly && (
+        <p
+          className={styles.info_line}
+        >{`Space: ${TAB_SPACES}, Ln ${cursorCount.row}, Col ${cursorCount.col}, Ch ${code.length}`}</p>
+      )}
+    </>
   );
 };
 
