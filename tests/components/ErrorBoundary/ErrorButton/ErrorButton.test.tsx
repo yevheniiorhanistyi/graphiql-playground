4c2b7621d5a6fef87c647aeba79882d63e41ac96
Yevhenii Orhanistyi <organistiy@gmail.com>
2024-01-07 10:58:12 +0100

    feat: add LocaleProvider wrapper

--- tests/components/ErrorBoundary/ErrorButton/ErrorButton.test.tsx
+++ tests/components/ErrorBoundary/ErrorButton/ErrorButton.test.tsx
@@ -1,17 +1,26 @@
 import { describe, test, expect } from 'vitest';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { ErrorButton } from '@/components/ErrorBoundary/ErrorButton/ErrorButton';
+import LocaleProvider from '@/localization/LocaleProvider';
 import { SMTH_WENT_WRONG } from '@/constants/stringConstants';
 
 describe('ErrorButton component', () => {
   test('renders ErrorButton component without error', () => {
-    render(<ErrorButton />);
+    render(
+      <LocaleProvider>
+        <ErrorButton />
+      </LocaleProvider>
+    );
 
     expect(screen.getByText('Show Error')).toBeInTheDocument();
   });
 
   test('throws error when button is clicked', () => {
-    render(<ErrorButton />);
+    render(
+      <LocaleProvider>
+        <ErrorButton />
+      </LocaleProvider>
+    );
 
     expect(() => {
       fireEvent.click(screen.getByText('Show Error'));
